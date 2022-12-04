2e05a1ed0f25f176df70dbbd951b132cb0ffd208
taotie张 <7856887+taotie_zhang@user.noreply.gitee.com>
2022-12-04 10:28:13 +0800

    20221204

---
+++ src/main/java/com/zhangbo/pojo/TabOutcome.java
@@ -0,0 +1,97 @@
+package com.zhangbo.pojo;
+
+
+public class TabOutcome {
+
+  private long purchaseId;
+  private String purchaseName;
+  private String purchaseTenderer;
+  private String purchaseTenderMethod;
+  private String purchaseOutcome;
+  private String purchaseCompanyName;
+  private String purchaseContact;
+  private String purchasePhone;
+  private String purchaseEmail;
+
+
+  public long getPurchaseId() {
+    return purchaseId;
+  }
+
+  public void setPurchaseId(long purchaseId) {
+    this.purchaseId = purchaseId;
+  }
+
+
+  public String getPurchaseName() {
+    return purchaseName;
+  }
+
+  public void setPurchaseName(String purchaseName) {
+    this.purchaseName = purchaseName;
+  }
+
+
+  public String getPurchaseTenderer() {
+    return purchaseTenderer;
+  }
+
+  public void setPurchaseTenderer(String purchaseTenderer) {
+    this.purchaseTenderer = purchaseTenderer;
+  }
+
+
+  public String getPurchaseTenderMethod() {
+    return purchaseTenderMethod;
+  }
+
+  public void setPurchaseTenderMethod(String purchaseTenderMethod) {
+    this.purchaseTenderMethod = purchaseTenderMethod;
+  }
+
+
+  public String getPurchaseOutcome() {
+    return purchaseOutcome;
+  }
+
+  public void setPurchaseOutcome(String purchaseOutcome) {
+    this.purchaseOutcome = purchaseOutcome;
+  }
+
+
+  public String getPurchaseCompanyName() {
+    return purchaseCompanyName;
+  }
+
+  public void setPurchaseCompanyName(String purchaseCompanyName) {
+    this.purchaseCompanyName = purchaseCompanyName;
+  }
+
+
+  public String getPurchaseContact() {
+    return purchaseContact;
+  }
+
+  public void setPurchaseContact(String purchaseContact) {
+    this.purchaseContact = purchaseContact;
+  }
+
+
+  public String getPurchasePhone() {
+    return purchasePhone;
+  }
+
+  public void setPurchasePhone(String purchasePhone) {
+    this.purchasePhone = purchasePhone;
+  }
+
+
+  public String getPurchaseEmail() {
+    return purchaseEmail;
+  }
+
+  public void setPurchaseEmail(String purchaseEmail) {
+    this.purchaseEmail = purchaseEmail;
+  }
+
+}
---
+++ src/main/java/com/zhangbo/pojo/User.java
@@ -0,0 +1,30 @@
+package com.zhangbo.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@TableName("tab_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = -1;
+    @TableId
+    private String userId;
+    private String userName;
+    private String userPassword;
+    private String userPhone;
+    private String userType;
+    private String userEmail;
+    private String userCompanyName;
+    private String userStatus;
+    private String userContactName;
+    private String userAptitude;
+}
