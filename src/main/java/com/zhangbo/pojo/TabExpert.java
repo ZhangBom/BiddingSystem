2264243b7a8ff6dafce5aebe5d8ab0e226376c37
taotie张 <7856887+taotie_zhang@user.noreply.gitee.com>
2023-02-03 16:04:10 +0800

    20221204

---
+++ src/main/java/com/zhangbo/pojo/TabExpert.java
@@ -0,0 +1,34 @@
+package com.zhangbo.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@TableName("tab_expert")
+public class TabExpert {
+  private static final long serialVersionUID = -1;
+  @TableId
+  private long expertId;
+  private String expertName;
+  private String expertPs;
+  private String expertGraduation;
+  private String expertOcupation;
+  private String expertSite;
+  private String expertPhone;
+  private String expertEmail;
+  private String expertIntroduce;
+  private String expertAccount;
+  private String expertStatus;
+  private String expertAccountStatus;
+  private String expertIndustry;
+
+
+}
---
+++ src/main/java/com/zhangbo/pojo/TabVendor.java
@@ -0,0 +1,31 @@
+package com.zhangbo.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@TableName("tab_vendor")
+public class TabVendor {
+  private static final long serialVersionUID = -1;
+  @TableId
+  private String vendorId;
+  private String vendorName;
+  private String vendorType;
+  private String vendorPhone;
+  private String vendorEmail;
+  private String vendorLevel;
+  private String vendorAptitude;
+  private String vendorAccount;
+  private String vendorContactName;
+  private String vendorContactPhone;
+  private String vendorContactEmail;
+  private String vendorStatus;
+  private String vendorAccountStatus;
+}
