b6bd1cb1fb10bccfccc5e800d81ed15d37c8b091
taotie张 <7856887+taotie_zhang@user.noreply.gitee.com>
2023-02-16 17:44:27 +0800

    20221204

---
+++ src/main/java/com/zhangbo/pojo/TabOpt.java
@@ -0,0 +1,23 @@
+package com.zhangbo.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@TableName("tab_opt")
+public class TabOpt {
+  private static final long serialVersionUID = -1;
+  @TableId
+  private long id;
+  private String type;
+  private String name;
+
+
+}
---
+++ src/main/java/com/zhangbo/pojo/TabStructure.java
@@ -0,0 +1,22 @@
+package com.zhangbo.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@TableName("tab_structure")
+public class TabStructure {
+  private static final long serialVersionUID = -1;
+  @TableId
+  private long strId;
+  private String tabName;
+  private String fieldName;
+  private String fieldNameCh;
+}
