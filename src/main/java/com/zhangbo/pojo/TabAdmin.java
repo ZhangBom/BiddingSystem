7dc35249020797bedd3bbb87db0926c596b60035
taotie张 <7856887+taotie_zhang@user.noreply.gitee.com>
2023-02-21 12:16:15 +0800

    20221204

---
+++ src/main/java/com/zhangbo/pojo/TabAdmin.java
@@ -0,0 +1,28 @@
+package com.zhangbo.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@TableName("tab_admin")
+public class TabAdmin {
+  private static final long serialVersionUID = -1;
+  @TableId
+  private String adminId;
+  private String adminAccount;
+  private String employeesId;
+  private String employeesName;
+  private String employeesPhone;
+  private String employeeEmail;
+
+
+
+
+}
